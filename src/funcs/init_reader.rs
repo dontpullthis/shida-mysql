9fdc784496e26d506029508b082cec72fd8046f0
Sergei Belyakov <1899416+tasmanianfox@users.noreply.github.com>
2021-06-09 00:57:30 +0300

    Parameters are passed from CLI. Need to clean up the code

--- src/funcs/init_reader.rs
+++ src/funcs/init_reader.rs
@@ -6,9 +6,74 @@ use shida_core::ffi;
 
 use crate::context::reader::ReaderContext;
 
-pub fn init_reader(_paramsc: ffi::Size, _paramsv: *const ffi::ConstCCharPtr) -> (*const u8, ffi::ConstCCharPtr) {
-    let url = "mysql://myuser:1234@localhost:3306/sample_db";
+struct MysqlConnectParams {
+    dbname: Option<String>,
+    host: Option<String>,
+    password: Option<String>,
+    port: Option<String>,
+    user: Option<String>,
+}
 
+impl MysqlConnectParams {
+    fn new() -> MysqlConnectParams {
+        MysqlConnectParams {
+            dbname: None,
+            host: None,
+            password: None,
+            port: None,
+            user: None,
+        }
+    }
+}
+
+fn format_separated_string(args: [&Option<String>; 2]) -> String {
+    Vec::from(args).iter()
+        .filter(|i| i.is_some())
+        .map(|i| i.as_ref().unwrap().clone())
+        .collect::<Vec<String>>()
+        .join(":")
+}
+
+fn format_url(params: &MysqlConnectParams) -> String {
+    let userpass = format_separated_string([&params.user, &params.password]);
+    let hostport = format_separated_string([&params.host, &params.port]);
+    let dbname = match &params.dbname {
+        Some(d) => d.clone(),
+        None => String::new(),
+    };
+
+    format!("mysql://{}@{}/{}", userpass, hostport, dbname)
+}
+
+pub fn init_reader(paramsc: ffi::Size, paramsv: *const ffi::ConstCCharPtr) -> (*const u8, ffi::ConstCCharPtr) {
+    let mut params = MysqlConnectParams::new();
+    for i in 0..paramsc {
+        let ch: ffi::ConstCCharPtr = unsafe { *paramsv.offset(i as isize) };
+        let param = unsafe {
+            match ffi::ccharptr_to_string(ch) {
+                Ok(string) => string,
+                Err(_) => return (std::ptr::null(), ffi::string_to_ccharptr(String::from("Failed to convert param"))),
+            }
+        };
+
+        let pos = match param.find("=") {
+            Some(position) => position,
+            None => continue,
+        };
+        let key = &param[..pos];
+        let value = String::from(&param[pos+1..]);
+
+        match key {
+            "database" => { params.dbname = Some(value); },
+            "host" => { params.host = Some(value); },
+            "password" => { params.password = Some(value); },
+            "port" => { params.port = Some(value); },
+            "user" => { params.user = Some(value); },
+            _ => {},
+        };
+    }
+
+    let url = format_url(&params);
     let pool = match Pool::new(url) {
         Ok(p) => p,
         Err(_) => return unsafe { 
@@ -18,8 +83,8 @@ pub fn init_reader(_paramsc: ffi::Size, _paramsv: *const ffi::ConstCCharPtr) -> 
     
     match pool.get_conn() {
         Ok(conn) => {
-            let connection = Box::from(ReaderContext::new(conn));
-            (unsafe { mem::transmute(connection) }, std::ptr::null())
+            let context = Box::from(ReaderContext::new(conn));
+            (unsafe { mem::transmute(context) }, std::ptr::null())
         },
         Err(_) => unsafe { ( std::ptr::null(), ffi::string_to_ccharptr(String::from("Failed to get mysql connection")) ) },
     }
